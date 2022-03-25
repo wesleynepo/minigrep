54be5478b2c5b21585a9cb6659a078c044df612f
wesleynepo <wesleynepomuceno1@gmail.com>
2022-03-24 23:31:56 -0300

    feat: split into lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
    let args: Vec<String> = env::args().collect();
@@ -13,26 +14,8 @@ fn main() {
    println!("Searching for {}", config.query);
    println!("In file {}", config.filename);
 
-   let contents =
-      fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-
-   println!("With text: \n{}", contents);
-}
-
-struct Config {
-   query: String,
-   filename: String,
-}
-
-impl Config {
-   fn new(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-         return Err("Not enough arguments");
-      }
-
-      let query = args[1].clone();
-      let filename = args[2].clone();
-
-      Ok(Config { query, filename })
+   if let Err(e) = minigrep::run(config) {
+      println!("Application error: {}", e);
+      process::exit(1);
    }
 }
